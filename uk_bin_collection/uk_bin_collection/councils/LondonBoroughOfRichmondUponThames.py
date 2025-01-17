b7d2b24f0b06b8202657b51f44b7542c52f5f042
Basavaraj Kalloli <basavarajkalloli@gmail.com>
2025-01-17 16:20:19 +0000

    signed commit?

--- uk_bin_collection/uk_bin_collection/councils/LondonBoroughOfRichmondUponThames.py
+++ uk_bin_collection/uk_bin_collection/councils/LondonBoroughOfRichmondUponThames.py
@@ -100,3 +100,5 @@ class CouncilClass(AbstractGetBinDataClass):
 
 CouncilClass().parse_data("", url="https://www.richmond.gov.uk/services/waste_and_recycling/collection_days/",
                           paon="March Road", headless=True)
+
+
